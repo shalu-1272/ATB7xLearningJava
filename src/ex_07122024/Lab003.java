e9d1bd94773f3795a4d7560c9fe0759a94e1376d
shalu-1272 <bansal.shalu19@gmail.com>
2024-07-12 16:48:46 +0530

    Commit all the 6 lab exercises

--- src/ex_07122024/Lab003.java
+++ src/ex_07122024/Lab003.java
@@ -3,7 +3,7 @@ package ex_07122024;
 public class Lab003 {
     public static void main(String[] args) {
         //Single Comment - Comment is used for describing what is written
-        /**Multiple comment
+        /*Multiple comment
          * Documentation purpose
          * You can comment out multiple non working or not required lines of the codes
          **/
