e1248ffd6f23b48b9617d5c491a774baf669ff90
shalu-1272 <bansal.shalu19@gmail.com>
2024-07-12 16:50:32 +0530

    Commit all the 6 lab exercises

--- src/ex_07122024/Lab004.java
+++ src/ex_07122024/Lab004.java
@@ -6,7 +6,7 @@ public class Lab004 {
         //Package -> Class -> Main Method -> Statements
         System.out.println(2+2);
         System.out.println(2*2);
-        System.out.println(2/2);
+       // System.out.println(2/2);
         System.out.println(2-2);
         System.out.println(3/2);  //understand integar
         //println - used to print on the console
